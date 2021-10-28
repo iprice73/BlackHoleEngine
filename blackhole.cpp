96ed1149def590b7632c464dcc8ca926a33ac1bb
iprice73 <mateusz.kowalski074@gmail.com>
2021-10-28 21:45:04 +0200

    set get blackhole

--- blackhole.cpp
+++ blackhole.cpp
@@ -1,9 +1,11 @@
 #include "blackhole.h"
 
-BlackHole::BlackHole(float mass, float r_s, float rot, float q) : mass_(mass),
-                                                                  schwarzchildRadius_(r_s),
-                                                                  rotation_(rot),
-                                                                  charge_(q)
+BlackHole::BlackHole(const QPointF& pos, float mass, float r_s, float rot, float q) :
+    pos_(pos),
+    mass_(mass),
+    schwarzchildRadius_(r_s),
+    rotation_(rot),
+    charge_(q)
 {
 
 }
--- blackhole.h
+++ blackhole.h
@@ -2,17 +2,27 @@
 #define BLACKHOLE_H
 
 #include <QObject>
+#include <QPointF>
 
 class BlackHole : public QObject
 {
 public:
-    BlackHole(float mass, float r_s, float rot, float q);
+    BlackHole(const QPointF& pos, float mass, float r_s, float rot, float q);
+
+    QPointF getPos() const { return pos_; }
+    float getMass() const { return mass_; }
+    float getRsch() const { return schwarzchildRadius_; }
+    float getRot() const { return rotation_; }
+    float getCharge() const { return charge_; }
 
 private:
+    QPointF pos_;
     float mass_;
     float schwarzchildRadius_;
     float rotation_;
     float charge_;
+
+
 };
 
 #endif // BLACKHOLE_H
